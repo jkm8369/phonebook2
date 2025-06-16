5da7fde2e3a72a98bb6e38c2618f4c27a0a4a7ff
jkm8369 <jkm8369@naver.com>
2025-06-16 17:39:32 +0900

    2025-06-16

---
+++ src/main/java/com/javaex/dao/PhonebookDAO.java
@@ -0,0 +1,121 @@
+package com.javaex.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.javaex.vo.PersonVO;
+
+public class PhonebookDAO {
+
+    //필드
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/phonebook_db";
+	private String id = "phonebook";
+	private String pw = "phonebook";
+
+    //생성자
+	public void phonebookDAO() {
+		
+	}
+
+    //메소드gs
+    //자원정리 메소드 - 공통
+	// DB연결 메소드
+	private void connect() { // 메인에서는 사용하지 못함
+
+		try {
+			// 1. JDBC 드라이버 (MySQL) 로딩
+			Class.forName(driver);
+
+			// 2. Connection 얻어오기
+			this.conn = DriverManager.getConnection(url, id, pw);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+	}
+	
+	private void close() {
+		// 5. 자원정리
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		
+	}
+
+    //전체리스트 가져오기
+	public List<PersonVO> personSelect() {
+		System.out.println("personSelect()");
+		List<PersonVO> personList = new ArrayList<PersonVO>();
+		
+		this.connect();
+		
+		try {
+			
+			//SQL문 준비
+			String query = "";
+			query += " select person_id, ";
+			query += " 		  name, ";
+			query += " 		  hp, ";
+			query += " 		  company ";
+			query += " from person ";
+			query += " order by person_id asc ";
+			
+			pstmt = conn.prepareStatement(query);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int personId = rs.getInt("person_id");
+				String name = rs.getString("name");
+				String hp = rs.getString("hp");
+				String company = rs.getString("company");
+				
+				PersonVO personVO = new PersonVO(personId, name, hp, company);
+				
+				personList.add(personVO);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error" + e);
+		}
+		
+		
+		this.close();
+		return personList;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
